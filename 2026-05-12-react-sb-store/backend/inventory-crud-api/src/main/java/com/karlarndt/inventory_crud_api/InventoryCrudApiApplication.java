847471c2e5835d8a38d6ea0096fb17b5079c5a7f
karl-arndt <157114758+karl-arndt@users.noreply.github.com>
2026-06-17 18:55:23 -0400

    chore: init Spring Boot backend

---
+++ 2026-05-12-react-sb-store/backend/inventory-crud-api/src/main/java/com/karlarndt/inventory_crud_api/InventoryCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.karlarndt.inventory_crud_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryCrudApiApplication.class, args);
+	}
+
+}
---
+++ 2026-05-12-react-sb-store/backend/inventory-crud-api/src/test/java/com/karlarndt/inventory_crud_api/InventoryCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karlarndt.inventory_crud_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
