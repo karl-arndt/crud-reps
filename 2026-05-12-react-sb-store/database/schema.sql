e8858988c244e4115f34d9e086bedc7005cc975d
karl-arndt <157114758+karl-arndt@users.noreply.github.com>
2026-06-17 15:29:48 -0400

    initialize database schema

---
+++ 2026-05-12-react-sb-store/database/schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE Customer (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    address VARCHAR(255),
+    phone VARCHAR(20)
+);
+
+CREATE TABLE Inventory (
+    id SERIAL PRIMARY KEY,
+    product_name VARCHAR(100),
+    quantity INT,
+    price DECIMAL(10, 2)
+);
+
+CREATE TABLE Orders (
+    id SERIAL PRIMARY KEY,
+    customer_id INT REFERENCES Customer(id),
+    created_at TIMESTAMP NOW()
+);
+
+CREATE TABLE OrderItem (
+    id SERIAL PRIMARY KEY,
+    order_id INT REFERENCES Orders(id),
+    inventory_id INT REFERENCES Inventory(id),
+    quantity INT,
+    price DECIMAL(10, 2)
+);
